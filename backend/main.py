0337aed40493b4d20d3d21ea66fe5c722cd3bfd2
Omar Flores <omar2003flores@gmail.com>
2026-04-22 05:24:28 -0400

    Add document ingestion and chunking pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from backend.routes.chat import router as chat_router
+from backend.services.ingestion_service import load_and_chunk_documents
 
 app = FastAPI()
 app.include_router(chat_router)
@@ -7,3 +8,9 @@ app.include_router(chat_router)
 @app.get("/")
 def home():
     return {"status": "API running"}
+
+# test the chunking system
+@app.get("/test-ingestion")
+def test_ingestion():
+    chunks = load_and_chunk_documents()
+    return {"chunks": chunks}
---
+++ backend/services/ingestion_service.py
@@ -0,0 +1,70 @@
+import os
+
+DATA_PATH = "data/"
+
+# Read files from /data and load the text into memory, the goal is to essentially provide useful context/data
+# to the llm, thus provide better quality responses.
+
+# read all files within /data, load them into memory, and return a list of docs
+def load_documents():
+    """
+    Reads all files from the data directory and returns their content as a list of strings.
+    Where each file = one document.
+    """
+    documents = []
+    # Loop through every file in /data
+    for filename in os.listdir(DATA_PATH):
+        filepath = os.path.join(DATA_PATH, filename)
+
+         # Make sure to focus on file types
+        if os.path.isfile(filepath):
+            with open(filepath, "r", encoding="utf-8") as f:
+                content = f.read()
+                # Add the full document text to our list
+                documents.append(content)
+
+    return documents
+
+# Need to split documents into data chunks, this makes it easier for an LLM to digest info, as well
+# as get relevant info based on certain chunks rather than getting all of them.
+
+def chunk_text(text, chunk_size=200):
+    """
+    Splits a large piece of text into smaller chunks.
+    So that:
+    - LLMs and embeddings work better with smaller text segments
+    - Later, each chunk will become an embedding
+
+    chunk_size = number of words per chunk
+    """
+    words = text.split() # Split text into individual words
+    chunks = []
+
+     # Loop through words in steps of chunk_size
+    for i in range(0, len(words), chunk_size):
+        # Take a slice of words and join them back into a string
+        chunk = " ".join(words[i:i + chunk_size])
+        # Add this chunk to list
+        chunks.append(chunk)
+
+    return chunks
+
+
+def load_and_chunk_documents():
+    """
+    Full pipeline:
+    1. Load documents
+    2. Break each document into chunks
+    3. Return ALL chunks in one list
+    """
+    docs = load_documents()
+    all_chunks = []
+
+    # Process each document individually
+    for doc in docs:
+        chunks = chunk_text(doc)
+
+        # Add all chunks from this document into the master list (all_chunks)
+        all_chunks.extend(chunks)
+
+    return all_chunks
